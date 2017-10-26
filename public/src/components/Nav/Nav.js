c86cebf8e551e9dfcb94f1c992fd60c483c2cb76
Gavin Foster <gavinnfoster@gmail.com>
2017-10-26 00:46:13 -0500

    add basic nav styles

--- public/src/components/Nav/Nav.js
+++ public/src/components/Nav/Nav.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
 
   return (
-    <div>
-      <h1><Link to={`/`}>www.officeparkcity.com</Link></h1>
-      <ul>
-        <li><Link to={`/about`}>about</Link></li>
-        <li><Link to={`/products`}>products</Link></li>
-        <li><Link to={`/contact`}>contact</Link></li>
-        <li><Link to={`/misc`}>misc</Link></li>
+    <div className="nav__container">
+      <h1 className="nav__title"><Link to={`/`}>www.officeparkcity.com</Link></h1>
+      <ul className="nav__primary">
+        <li className="nav__list-item"><Link to={`/about`}>about</Link></li>
+        <li className="nav__list-item"><Link to={`/products`}>products</Link></li>
+        <li className="nav__list-item"><Link to={`/contact`}>contact</Link></li>
+        <li className="nav__list-item"><Link to={`/misc`}>misc</Link></li>
       </ul>
     </div>
   );
